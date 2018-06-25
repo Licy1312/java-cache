a10967e6358b253fac7418151edf951129d841ac
jinmu <jinmu@baofu.com>
2018-06-25 18:03:23 +0800

    ehcache commit

---
+++ src/main/java/com/learning/cache/ehcache/EhcacheManager.java
@@ -0,0 +1,17 @@
+package com.learning.cache.ehcache;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/**
+ * Description: ehcacheManger
+ *
+ * @Author:Jinmu Date: 2018/6/19
+ */
+public class EhcacheManager {
+
+    public void set(){
+        Hashtable<String,Integer> hashtable = new Hashtable<>();
+        HashMap<String,Integer> hashMap = new HashMap<>();
+    }
+}
